8fd0332057d8d885a57e5f5e24992d12e0de3cf9
Marco Parra <maaferna@gmail.com>
2022-04-27 18:27:01 -0400

    Blog Post comment, ready

--- blango/urls.py
+++ blango/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", blog.views.index),
+        path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+        
 ]

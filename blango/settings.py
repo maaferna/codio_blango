49a105375413a56e5e696a9c90585b0c2992ca4b
Marco Parra <maaferna@gmail.com>
2022-05-02 21:11:42 -0400

    django built security features provides

--- blango/settings.py
+++ blango/settings.py
@@ -177,3 +177,12 @@ LOGGING = {
 
 ADMINS = [("Marco Parra", "maaferna@gmail.com"), ("Leo Lucio",
 "leo@example.com")]
+
+PASSWORD_HASHERS = [
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+'django.contrib.auth.hashers.Argon2PasswordHasher',
+'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
+

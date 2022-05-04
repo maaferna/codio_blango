05eef498c52ab6c53860634c0cad8741828de148
Marco Parra <maaferna@gmail.com>
2022-05-04 16:21:30 -0400

    add lower level cache option

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,7 +4,9 @@ from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from blog.models import Post
+import logging
 
+logger = logging.getLogger(__name__)
 
 user_model = get_user_model()
 register = template.Library()
@@ -76,4 +78,6 @@ def endcol():
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts),post.pk)   
     return {"title": "Recent Posts", "posts": posts}
+    

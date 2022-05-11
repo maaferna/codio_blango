fc86c5ffb5248948ecb19633521887ab0f0a0cb6
Marco Parra <maaferna@gmail.com>
2022-05-11 16:03:11 -0400

    Add mail connection to loggin

--- blango_auth/admin.py
+++ blango_auth/admin.py
@@ -2,5 +2,33 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from blango_auth.models import User
 # Register your models here.
+from django.utils.translation import gettext_lazy as _
+#admin.site.register(User, UserAdmin)
+'''
+The last change to the ﬁle is to register BlangoUserAdmin as the admin class
+for User . Change the line of code that says admin.site.register(User,
+UserAdmin to:
+'''
 
-admin.site.register(User, UserAdmin)
+
+class BlangoUserAdmin(UserAdmin):
+	fieldsets = (
+	(None, {"fields": ("email", "password")}), (_("Personal info"), {"fields": ("first_name", "last_name")}), 		(
+		_("Permissions"), 
+		{
+		"fields": ( "is_active", "is_staff", "is_superuser", "groups", "user_permissions",)
+		},
+	 ), 
+	 (_("Important dates"), {"fields": ("last_login","date_joined")}),
+	)
+	add_fieldsets = (
+	(None, 
+	{
+		"classes": ("wide",),"fields": ("email", "password1", "password2"),
+	},
+		),)
+	list_display = ("email", "first_name", "last_name", "is_staff")
+	search_fields = ("email", "first_name", "last_name")
+	ordering = ("email",)
+
+admin.site.register(User, BlangoUserAdmin)

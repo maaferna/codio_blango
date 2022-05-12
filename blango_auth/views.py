6d7e9023f54e706102e0e0f106865772ed442205
Marco Parra <maaferna@gmail.com>
2022-05-11 20:06:18 -0400

    add accounts registration

--- blango_auth/views.py
+++ blango_auth/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile(request):
+	return render(request, "blango_auth/profile.html")

c6160436c253188066cc80aa6f17bb53ed47f02b
Marco Parra <maaferna@gmail.com>
2022-05-12 17:50:36 -0400

    switch

--- blango/urls.py
+++ blango/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("ip/", blog.views.get_ip),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/profile/", blango_auth.views.profile,name="profile"),
-    path("accounts/register/",RegistrationView.as_view(form_class=BlangoRegistrationForm),name="django_registration_register",),
+    path("accounts/register/",RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register",),
 	path("accounts/",include("django_registration.backends.activation.urls")),
 ]
 

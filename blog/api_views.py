cf7b42a426227a8c93f221749fdfef696fa2c94a
Marco Parra <maaferna@gmail.com>
2022-05-21 19:35:46 -0400

    Add funcionality of Rest Framework to GET PUT POST

--- blog/api_views.py
+++ blog/api_views.py
@@ -5,8 +5,9 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from blog.models import Post
+from blog.api.serializers import PostSerializer
 
-
+'''
 def post_to_dict(post):
 	return {
 		"pk": post.pk,
@@ -19,16 +20,22 @@ def post_to_dict(post):
 		"summary": post.summary,
 		"content": post.content,
 		}
+'''
+
 		
 @csrf_exempt
 def post_list(request):
 	if request.method == "GET":
 		posts = Post.objects.all()
-		posts_as_dict = [post_to_dict(p) for p in posts]
-		return JsonResponse({"data": posts_as_dict})
+		#posts_as_dict = [post_to_dict(p) for p in posts]
+		#return JsonResponse({"data": posts_as_dict})
+		return JsonResponse({"data": PostSerializer(posts, many=True).data})
 	elif request.method == "POST":
 		post_data = json.loads(request.body)
-		post = Post.objects.create(**post_data)
+		#post = Post.objects.create(**post_data)
+		serializer = PostSerializer(data=post_data)
+		serializer.is_valid(raise_exception=True)
+		post = serializer.save()
 		return HttpResponse(status=HTTPStatus.CREATED,headers={"Location": reverse("api_post_detail", args=(post.pk,))},)
 	
 	return HttpResponseNotAllowed(["GET", "POST"])
@@ -39,13 +46,20 @@ def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	
 	if request.method == "GET":
-		return JsonResponse(post_to_dict(post))
+		#return JsonResponse(post_to_dict(post))
+		return JsonResponse(PostSerializer(post).data)
 	elif request.method == "PUT": 
 		post_data = json.loads(request.body)
+		serializer = PostSerializer(post, data=post_data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return HttpResponse(status=HTTPStatus.NO_CONTENT)
+		'''
 		for field, value in post_data.items():
 			setattr(post, field, value)
 			post.save()
 			return HttpResponse(status=HTTPStatus.NO_CONTENT)
+		'''
 	elif request.method == "DELETE":
 		post.delete()
 		return HttpResponse(status=HTTPStatus.NO_CONTENT)

fa8bd6bc5f6b7c55390cd70c3ddc9494b6adfdba
Marco Parra <maaferna@gmail.com>
2022-05-29 21:04:06 -0400

    Add combinate permisions classes

--- blog/api/permissions.py
+++ blog/api/permissions.py
@@ -6,4 +6,8 @@ class AuthorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
             return True
 
         return request.user == obj.author
+
+class IsAdminUserForObject(permissions.IsAdminUser):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user and request.user.is_staff)
         
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from blog.api.serializers import PostSerializer
 from blog.models import Post
-from blog.api.permissions import AuthorModifyOrReadOnly
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 
 class PostList(generics.ListCreateAPIView):
@@ -9,6 +9,6 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [AuthorModifyOrReadOnly]
+    permission_classes = [AuthorModifyOrReadOnly| IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer

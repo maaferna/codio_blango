5cbe0c66b6adfa090e07128aab61839765e23475
Marco Parra <maaferna@gmail.com>
2022-05-29 20:49:33 -0400

    Add Custom Permission Classes

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from blog.api.serializers import PostSerializer
 from blog.models import Post
+from blog.api.permissions import AuthorModifyOrReadOnly
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer

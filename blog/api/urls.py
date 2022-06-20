6e9264b130b500bd244b023beff118c211afe11d
Marco Parra <maaferna@gmail.com>
2022-06-20 19:47:33 -0400

    Add posts by-time attribute, error with datatabase

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -32,6 +32,8 @@ urlpatterns += [
     path("token-auth/", views.obtain_auth_token),
     re_path(r"^swagger(?P<format>\.json|\.yaml)$",schema_view.without_ui(cache_timeout=0),name="schema-json",),
     path("swagger/",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui",),
+    path("", include(router.urls)),
+    path("posts/by-time/<str:period_name>/",PostViewSet.as_view({"get": "list"}),name="posts-by-time",),
 ]
 
 
--- blog/api/views.py
+++ blog/api/views.py
@@ -14,10 +14,12 @@ from blango_auth.models import User
 from django.utils.decorators import method_decorator
 from django.utils import timezone
 from django.db.models import Q 
+from django.http import Http404
 
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie, vary_on_headers
 
+from datetime import timedelta
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -74,7 +76,8 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer = PostSerializer(posts, many=True, context={"request":request})
         return Response(serializer.data)
 
-    def get_queryset(self):            
+    def get_queryset(self):
+        '''
         if self.request.user.is_anonymous:
             #published only
             return self.queryset.filter(published_at__lte=timezone.now())
@@ -85,6 +88,27 @@ class PostViewSet(viewsets.ModelViewSet):
         
         #filter for own or
         return self.queryset.filter(Q(published_at__lte=timezone.now())| Q(author=self.request.user))
+        '''            
+        if self.request.user.is_anonymous:
+            # published only
+            queryset = self.queryset.filter(published_at__lte=timezone.now())
+        elif not self.request.user.is_staff:
+            # allow all
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(Q(published_at__lte=timezone.now()) |  Q(author=self.request.user))
+        time_period_name = self.kwargs.get("period_name")
+        if not time_period_name:
+        # no further filtering required
+            return queryset
+        if time_period_name == "new":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(hours=1))
+        elif time_period_name == "today":
+            return queryset.filter(published_at__date=timezone.now().date(),)
+        elif time_period_name == "week":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+        else:
+            raise Http404(f"Time period {time_period_name} is not valid, should be "f"'new', 'today' or 'week'")
     
     @method_decorator(cache_page(300))
     @method_decorator(vary_on_headers("Authorization", "Cookie"))

3e341bae16971f712c93edcabcea94cde35edf6f
Marco Parra <maaferna@gmail.com>
2022-07-05 14:31:22 -0400

    Add media root to versatile

--- blango/urls.py
+++ blango/urls.py
@@ -22,6 +22,8 @@ from django.urls import path, include
 import blango_auth.views
 from django_registration.backends.activation.views import RegistrationView
 from blango_auth.forms import BlangoRegistrationForm
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -40,6 +42,7 @@ urlpatterns = [
 
 if settings.DEBUG:
 	urlpatterns += [path("__debug__/", include(debug_toolbar.urls)),
-]
+] + static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
 
 

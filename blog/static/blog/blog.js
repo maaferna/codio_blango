0837f19a1e10dda41fbb61472e0fa52378203054
Marco Parra <maaferna@gmail.com>
2022-07-05 11:13:29 -0400

    javascripts with react add button to DOM

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,3 +1,42 @@
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false
+  }
+
+  handleClick () {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+  render () {
+    let buttonText
+
+    if (this.state.wasClicked)
+      buttonText = 'Clicked!'
+    else
+      buttonText = 'Click Me'
+
+    return React.createElement(
+      'button',
+      {
+        className: 'btn btn-primary mt-2',
+        onClick: () => {
+          this.handleClick()
+        },
+      },
+      buttonText
+    )
+  }
+}
+
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
+
+/* 
 function resolvedCallback(data) {
   console.log('Resolved with data ' +  data)
 }
@@ -26,7 +65,7 @@ lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
 
 
 
-/* 
+
 class Greeter {
   constructor (name) {
     this.name = name
